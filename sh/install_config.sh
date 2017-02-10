aa75dd6ebc2bd25a4e373f375deb88e20cb45eb1
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-02-10 14:08:48 +0100

    Updated paths

--- sh/install_config.sh
+++ sh/install_config.sh
@@ -6,8 +6,8 @@ source $CDF2CIM_WS_HOME/sh/utils.sh
 # Main entry point.
 main()
 {
-	cp $CDF2CIM_WS_HOME/resources/template-supervisord.conf $CDF2CIM_WS_HOME/ops/config/supervisord.conf
-	cp $CDF2CIM_WS_HOME/resources/template-ws.conf $CDF2CIM_WS_HOME/ops/config/ws.conf
+	cp $CDF2CIM_WS_HOME/resources/supervisord.conf $CDF2CIM_WS_HOME/ops/config/supervisord.conf
+	cp $CDF2CIM_WS_HOME/resources/ws.conf $CDF2CIM_WS_HOME/ops/config/ws.conf
 
 	log "configuration files initialized"
 }

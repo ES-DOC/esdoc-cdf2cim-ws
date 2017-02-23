811b0d4664311c1c532833be7f77f03e78b759a0
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-02-23 13:44:57 +0100

    Minor change to dameon reload script

--- sh/daemon_reload.sh
+++ sh/daemon_reload.sh
@@ -8,8 +8,6 @@ main()
 {
 	source $CDF2CIM_WS_HOME/sh/daemon_stop.sh
 	source $CDF2CIM_WS_HOME/sh/daemon_start.sh
-	sleep 3.0
-	source $CDF2CIM_WS_HOME/sh/daemon_status.sh
 }
 
 # Invoke entry point.
--- sh/daemon_start.sh
+++ sh/daemon_start.sh
@@ -8,8 +8,10 @@ main()
 {
 	source $CDF2CIM_WS_HOME/sh/reset_logs.sh
 	supervisord -c $CDF2CIM_WS_HOME/ops/config/supervisord.conf
-
 	log "initialized web-service daemon"
+
+	sleep 3.0
+	source $CDF2CIM_WS_HOME/sh/daemon_status.sh
 }
 
 # Invoke entry point.
